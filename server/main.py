9ae78748c8cca24761cd71671974ec2610ef6153
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2026-03-25 20:22:41 +1100

    PAPER server in service!

--- server/main.py
+++ server/main.py
@@ -1,5 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
+import time
+import json
+import uuid
 import os
 import render
 
@@ -9,7 +12,12 @@ products = {
     "custom ball" : 1.5
 }
 
-import os
+LIMITS = {}
+
+def updateServerVars():
+    for i in LIMITS:
+        if(time.time() - LIMITS[i][1] > 1800):
+            del LIMITS[i]
 
 def openDB(loc, mode, content="", main=True):  
     if main:  
@@ -113,6 +121,8 @@ def openDB(loc, mode, content="", main=True):
             with open(path, m) as f:
                 f.write(content)
             return
+        
+order = json.loads(openDB("order.json", "read"))
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -152,6 +162,8 @@ class Handler(BaseHTTPRequestHandler):
         length = int(self.headers.get("Content-Length", 0))
         body = self.rfile.read(length)
 
+        ip = self.client_address[0]
+
         def get_reject():
             with open("./_site/pages/reject.html", "rt") as f:
                 return f.read()
@@ -175,7 +187,7 @@ class Handler(BaseHTTPRequestHandler):
             )
             return
 
-        data = parse_qs(body)
+        data = parse_qs(raw)
 
         for _, values in data.items():
             if len(values) != 1:
@@ -190,6 +202,10 @@ class Handler(BaseHTTPRequestHandler):
                 return
             
         data = {k: v[0] for k, v in data.items()}
+
+        if("admin" in data.keys()):
+            print(ip)
+            return
 
         if "type" not in data.keys():
             send_html(400, 
@@ -213,7 +229,54 @@ class Handler(BaseHTTPRequestHandler):
             )
             return
         
+        if "name" not in data.keys():
+            send_html(400, 
+                render.render(get_reject(), {
+                    "status" : 400, 
+                    "reason" : "Request was invalid or malformed", 
+                    "details" : "Name is missing. "
+                }).encode("utf-8", "ignore"), 
+                self
+            )
+            return
         
+        if(ip in LIMITS.keys()):
+            if(LIMITS.get(ip)[0] >= 10):
+                send_html(429, 
+                    render.render(get_reject(), {
+                        "status" : 429, 
+                        "reason" : "Too many requests", 
+                        "details" : "Too many orders in last 30 minutes. "
+                    }).encode("utf-8", "ignore"), 
+                    self
+                )
+                return
+            else:
+                LIMITS[ip] = [LIMITS.get(ip)[0]+1, time.time()]
+        else:
+            LIMITS[ip] = [1, time.time()]
+
+        if("priority" not in data.keys()):
+            priority = False
+        else:
+            priority = True
+        hight = 0
+        for i in order:
+            if(order[i]["pos"] == hight+1 and order[i]["priority"] == priority):
+                hight = order[i]["pos"]
+            elif(order[i]["pos"] > hight and order[i]["priority"] == priority):
+                order[i]["pos"] = hight+1
+                hight += 1
+        orderId = uuid.uuid4()
+        order[str(orderId)] = {
+            "priority" : priority, 
+            "pos" : hight, 
+            "name" : data.get("name"), 
+            "type" : data.get("type"), 
+            "price" : products.get(data.get("type"))
+        }
+
+        openDB("order.json", "write", json.dumps(order))
 
         with open("./_site/pages/success.html", "rt") as f:
             self.send_response(200)
